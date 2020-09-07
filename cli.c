6c4d08ebda527539f2accaaa82b8daffdb170ae0
chu4ng <egoralladinov@gmail.com>
2020-09-07 20:26:39 +0300

    Fix help message

--- cli.c
+++ cli.c
@@ -8,18 +8,21 @@
 const char *argp_program_version = "Version of tripcode: 0.2";
 const char *argp_program_bug_address = "<shichuang@horsefucker.org>";
 static char doc[] = "Maining tripcodes by pattern, count and case sensitivity";
-static char args_doc[] = "";
+static char args_doc[] = "-m PATTERN -c COUNT";
+static error_t parse_opt(int key, char *arg, struct argp_state *state);
 static struct argp_option options[] = { 
-	{ "match", 'm', "PATTERN", 0, "Pattern for match tripcodes."},
-	{ "count", 'c', "NUMBER", 0, "Number of getting tripcodes."},
-	{ "nocase",'i', 0, OPTION_ARG_OPTIONAL, "Ignor case for matched tripcodes"},
+	{ "match", 'm', "PATTERN", OPTION_NO_USAGE, "Pattern for match tripcodes."},
+	{ "count", 'c', "NUMBER", OPTION_NO_USAGE, "Number of getting tripcodes."},
+	{ "ignore_case",'i', 0, OPTION_ARG_OPTIONAL, "Ignor case for matched tripcodes"},
 	{ 0 } 
 };
+
+static struct argp argp= { options, parse_opt, args_doc, doc, 0, 0, 0 };
 
 struct arguments {
 	char *match_str;
 	int count;
-	bool caseIgnor;
+	bool ignoreCase;
 	uint8_t req;
 };
 
@@ -28,19 +31,18 @@ static error_t parse_opt(int key, char *arg, struct argp_state *state) {
 	switch (key) {
 		case 'm': arguments->match_str = arg; arguments->req += 1; break;
 		case 'c': arguments->count = atoi(arg);  arguments->req += 1; break;
-		case 'i': arguments->caseIgnor = true; break;
+		case 'i': arguments->ignoreCase = true; break;
 		case ARGP_KEY_ARG: return 0;
 		case ARGP_KEY_END:
-		   if (arguments->req < 2) {
+			if (arguments->req < 2) {
 			   argp_usage(state);
 			   printf("Too few arguments!\n");
-		   }
+		    }
 		default: return ARGP_ERR_UNKNOWN;
 	}   
 	return 0;
 }
 
-static struct argp argp = { options, parse_opt, args_doc, doc, 0, 0, 0 };
 #endif
 
 int main(int argc, char *argv[]) {
@@ -53,14 +55,14 @@ int main(int argc, char *argv[]) {
 #if DEV == 0
 	struct arguments arguments;
 
-    arguments.caseIgnor = false;
+    arguments.ignoreCase = false;
     arguments.req = 0;
 
     argp_parse(&argp, argc, argv, 0, 0, &arguments);	
 
 	match = arguments.match_str;
 	count = arguments.count;
-	ignor = arguments.caseIgnor;
+	ignor = arguments.ignoreCase;
 
 	seed = time(0);
 #else
